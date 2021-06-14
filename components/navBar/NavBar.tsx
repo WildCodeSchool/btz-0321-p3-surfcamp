fdda2a8792e6905862858e878ab647977fc15cb2
viccabezos <cabezosvic@gmail.com>
2021-06-14 13:59:04 +0200

    nav Bar

---
+++ components/navBar/NavBar.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function NavBar() {
+  return (
+    <div className="flex justify-between items-center px-4 h-20 w-full bg-BlueCamp text-white">
+      <Image
+        src="/images/surfCampLogo.png"
+        width={150}
+        height={55}
+        alt="logo"
+      />
+      <div></div>
+      <div className="flex items-center text-lg ">
+        <Image
+          src="/images/menu-outline.svg"
+          width={50}
+          height={50}
+          alt="logo"
+        />
+        <a className="px-2">login</a>
+        <Image
+          src="/images/francais.png"
+          width={30}
+          height={30}
+          alt="francais"
+          className="rounded-full"
+        />
+      </div>
+    </div>
+  );
+}
