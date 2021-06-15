4900d70217d4a4aa73356b30db4043eae92316b9
viccabezos <cabezosvic@gmail.com>
2021-06-15 10:43:09 +0200

    add navbar and fix Iprops

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import Head from "next/head";
+import NavBar from "../navBar/NavBar";
 
 interface IProps {
   children: React.ReactNode;
-  page?: string;
 }
 
-export default function layout({ page, children }: IProps) {
+export default function layout({ children }: IProps) {
   return (
     <div className="layout">
       <Head>
-        <title>{page}</title>
+        <title>Surf camp</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <NavBar />
       <div className="flex flex-col w-full min-h-screen bg-blue-300 ">
         {children}
       </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AppProps } from "next/app";
-import "tailwindcss/tailwind.css";
 import Layout from "../components/layout/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
