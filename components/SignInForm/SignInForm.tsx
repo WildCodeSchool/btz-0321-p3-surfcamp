6205fc174fa7784ae7a54f08dd9a0cdc0419aec5
rcarondw <r.carondw@gmail.com>
2021-06-17 10:09:25 +0200

    sign in form done

--- components/SignInForm/SignInForm.tsx
+++ components/SignInForm/SignInForm.tsx
@@ -1,42 +1,94 @@
-import { useForm } from 'react-hook-form'
+import React, { useEffect, useState } from "react";
+import Joi from "joi";
+import { useForm } from "react-hook-form";
+import { joiResolver } from "@hookform/resolvers/joi";
+
+const schema = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string().min(6).required(),
+  confirmPassword: Joi.any().valid(Joi.ref("password")).required(),
+});
 
 export default function SignInForm() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    resolver: joiResolver(schema),
+  });
 
-    const {register, formState:{errors}, handleSubmit}=useForm()
+  const onSubmit = (data: object) => {
+    console.log(data);
+  };
 
-    const onSubmit = (data:object) => {
-        console.log(data);
-        
-    }
-
-    return(
-        <div className="w-full h-screen bg-black bg-opacity-70 flex justify-center items-center">
-        <form className=" flex text-white items-center px-8 h-auto rounded-lg flex-col bg-BlueCamp border    border-white" onSubmit={handleSubmit(onSubmit)} >
-            <div className="text-center my-2 mt-8">
-            <h2 className="text-2xl mb-2">Inscription</h2>
-            <div className="border w-32 mx-auto"></div>
-            </div>
-            <div className="flex flex-col justify-center w-full">
-            <div className="flex flex-col my-2 text-sm">
-            <input className="h-8 w-64 rounded-lg  text-black mx-auto px-2" placeholder="Email..." type="email" id="email" {...register('email', {required: true})} />
-            <p className="text-white text-sm ">{errors.email?.type === 'required' && '*email is required'}</p>
-            </div>
-            <div className="flex flex-col my-2 text-sm" >
-            <input className="h-8 w-64 rounded-lg text-black mx-auto px-2" placeholder="Mot de passe..." type="password" id="password" {...register('password', {required: true})} />
-            <p className="text-white text-sm ">{errors.password?.type === 'required' && '*Password is required'}</p>
-            </div>
-            <div className="flex flex-col my-2 text-sm" >
-            <input className="h-8 w-64 rounded-lg text-black mx-auto px-2" placeholder="Confirmation du mot de passe..." type="password" id="password" {...register('password', {required: true})} />
-            <p className="text-white text-sm ">{errors.password?.type === 'required' && '*Password is required'}</p>
-            </div>
-         
-            </div>
-            <div className="flex flex-col text-center mt-2 mb-2">
-            <p className="text-xs">Déjà un compte ? Connect toi <span className="text-red-400">ici</span></p>
-
-            </div>
-            <button className="border py-1 w-28 rounded-lg my-2 mb-8" type="submit">Se connecter</button>
-        </form>
+  return (
+    <div>
+      <form
+        className=" flex text-white items-center px-8 h-auto rounded-lg flex-col bg-BlueCamp border    border-white"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="text-center my-2 mt-8">
+          <h2 className="text-2xl mb-2">Inscription</h2>
+          <div className="border w-32 mx-auto"></div>
+        </div>
+        <div className="flex flex-col justify-center w-full">
+          <div className="flex flex-col my-2 text-sm">
+            <input
+              className={`h-8 w-64 rounded-lg  text-black mx-auto px-2 border
+              ${errors.email && "border-red-600"}
+              `}
+              placeholder="Email..."
+              type="email"
+              id="email"
+              {...register("email", { required: true })}
+            />
+            <p className="text-white text-sm ">
+              {errors.email && "Veuillez saisir un email"}
+            </p>
+          </div>
+          <div className="flex flex-col my-2 text-sm">
+            <input
+              className={`
+              h-8 w-64 rounded-lg text-black mx-auto px-2 border
+              ${errors.password && "border-red-600"}
+              `}
+              placeholder="Mot de passe..."
+              type="password"
+              id="password"
+              {...register("password", { required: true })}
+            />
+            <p className="text-white text-sm ">
+              {errors.password && "Entrez au minimum 6 caractère"}
+            </p>
+          </div>
+          <div className="flex flex-col my-2 text-sm">
+            <input
+              className="h-8 w-64 rounded-lg text-black mx-auto px-2"
+              placeholder="Confirmation du mot de passe..."
+              type="password"
+              id="confirmPassword"
+              {...register("confirmPassword", { required: "password" })}
+            />
+            <p className="text-white text-sm ">
+              {errors.confirmPassword && "Confirmation invalide"}
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col text-center mt-2 mb-2">
+          <p className="text-xs">
+            Déjà un compte ? Connecte toi{" "}
+            <span className="text-red-400">ici</span>
+          </p>
+        </div>
+        <button
+          //   disabled={isDisabled}
+          className={`border py-1 w-28 rounded-lg my-2 mb-8`}
+          type="submit"
+        >
+          S'inscrire
+        </button>
+      </form>
     </div>
-    )
-};
+  );
+}
--- pages/signin.tsx
+++ pages/signin.tsx
@@ -2,7 +2,7 @@ import SignInForm from "../components/SignInForm/SignInForm"
 
 export default function signin() {
     return(
-        <div>
+        <div className="w-full h-screen bg-black bg-opacity-70 flex justify-center items-center">
             <SignInForm />
         </div>
     )
