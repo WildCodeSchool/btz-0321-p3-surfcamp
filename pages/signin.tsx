83f587fefb5f45c1a59d5c37892f01edc206bd27
rcarondw <r.carondw@gmail.com>
2021-06-18 11:09:37 +0200

    fix last eslint err

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,9 +1,9 @@
-import SignInForm from "../components/SignInForm/SignInForm"
+import SignInForm from "../components/SignInForm/SignInForm";
 
-export default function signin() {
-    return(
-        <div className="w-full h-screen bg-black bg-opacity-70 flex justify-center items-center">
-            <SignInForm />
-        </div>
-    )
-};
+export default function signin(): JSX.Element {
+  return (
+    <div className="w-full h-screen bg-black bg-opacity-70 flex justify-center items-center">
+      <SignInForm />
+    </div>
+  );
+}
