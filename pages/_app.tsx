176f9ad27679562013e48c272ed16c7ce94730f4
rcarondw <r.carondw@gmail.com>
2021-06-18 12:32:56 +0200

    added navBar component into layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
 import Footer from "../components/Footer/Footer";
 import Layout from "../components/layout/Layout";
+import NavBar from "../components/navBar/NavBar";
 import SearchBar from "../components/searchBar/SearchBar";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <Layout>
+      <NavBar />
       <Component {...pageProps} />
       <SearchBar />
       <Footer />
