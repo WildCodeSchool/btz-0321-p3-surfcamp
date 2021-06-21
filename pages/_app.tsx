ae2542fff7abe60d1f6d928f11a3d9cc12445301
rcarondw <r.carondw@gmail.com>
2021-06-21 15:53:42 +0200

    ossature plus state nombre voyageur du component filtre

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,13 @@ import Layout from "../components/layout/Layout";
 import NavBar from "../components/navBar/NavBar";
 import SearchBar from "../components/searchBar/SearchBar";
 import "../styles/globals.css";
+import Filter from "../components/Filter/Filter";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <Layout>
       <NavBar />
+      <Filter />
       <Component {...pageProps} />
       <SearchBar />
       <Footer />
