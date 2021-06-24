a0d8afcb5765d13a9d223b8fd7597cb1712bfb6c
JIdayyy <julien_abbadie@hotmail.fr>
2021-06-25 01:27:11 +0200

    prettier

--- components/Carroussel/Carroussel.tsx
+++ components/Carroussel/Carroussel.tsx
@@ -1,40 +1,41 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { useQuery } from "react-query";
-import Image from "next/image"
-import axios from 'axios';
+import Image from "next/image";
+import axios from "axios";
 
 interface IProps {
-    ressource: string;
+	ressource: string;
 }
 
-export default function Carroussel({ressource}: IProps): JSX.Element {
-    const [skipQuery, setSkipQuery] = useState(0)
-    const { error, data, refetch,isLoading } = useQuery(`${ressource}`, () => axios(`http://localhost:5000/${ressource}/?skip=${skipQuery}&take=4`));
+export default function Carroussel({ ressource }: IProps): JSX.Element {
+	const [skipQuery, setSkipQuery] = useState(0);
+	const { error, data, refetch, isLoading } = useQuery(`${ressource}`, () =>
+		axios(`http://localhost:5000/${ressource}/?skip=${skipQuery}&take=4`));
 
-    const forward = () => {
-        setSkipQuery(c => c += 4)
-        refetch()
-    }
-    const backward = () => {
-        if (skipQuery === 0) { setSkipQuery(0) }
-        else {
-            setSkipQuery(c => c -= 4)
-            refetch()
-        }
-    }
+	const forward = () => {
+		setSkipQuery(c => c += 4)
+		refetch()
+	}
+	const backward = () => {
+		if (skipQuery === 0) { setSkipQuery(0) }
+		else {
+			setSkipQuery(c => c -= 4)
+			refetch()
+		}
+	}
 
-    if (error) return <div>...error</div>
-    if (isLoading) return <div>...loading</div>
+	if (error) return <div>...error</div>
+	if (isLoading) return <div>...loading</div>
 
-    return (
-        <div className="w-full flex items-center align-middle justify-around h-full">
-            <button onClick={backward}>BACKWARD</button>
-            {
-                data && data.data.map((image: { url: string}, index: number) => {
-                    return <div><Image key={index} src={image.url} width={300} height={300} quality={100} /></div>
-                })
-            }
-            <button onClick={forward}>FORWARD</button>
-        </div>
-    )
+	return (
+		<div className="w-full flex items-center align-middle justify-around h-full">
+			<button onClick={backward}>BACKWARD</button>
+			{
+				data && data.data.map((image: { url: string }, index: number) => {
+					return <div><Image key={index} src={image.url} width={300} height={300} quality={100} /></div>;
+				})
+			}
+			<button onClick={forward}>FORWARD</button>
+		</div>
+	)
 }
