9e69b4e9dedf60e7bbbcb6bce44fe0a108710a99
JIdayyy <julien_abbadie@hotmail.fr>
2021-06-26 23:05:30 +0200

    carroussel props

--- components/Carroussel/Carroussel.tsx
+++ components/Carroussel/Carroussel.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useQuery } from "react-query";
 import Image from "next/image";
 import axios from "axios";
@@ -10,13 +10,20 @@ interface IProps {
 
 export default function Carroussel({ ressource, take }: IProps): JSX.Element {
   const [skipQuery, setSkipQuery] = useState(0);
+  const [animation, setAnimation] = useState("");
+  const [loading, setLoading] = useState(true);
+
   const { error, data, refetch, isLoading } = useQuery(`${ressource}`, () =>
     axios(`http://localhost:5000/${ressource}/?skip=${skipQuery}&take=${take}`)
   );
+
   const forward = () => {
+    setLoading(false);
     setSkipQuery((c) => (c += parseInt(take)));
     refetch();
+    setAnimation("");
   };
+
   const backward = () => {
     if (skipQuery === 0) {
       setSkipQuery(0);
@@ -26,16 +33,27 @@ export default function Carroussel({ ressource, take }: IProps): JSX.Element {
     }
   };
 
+  useEffect(() => {
+    setAnimation("scale-in-center");
+    setLoading(true);
+  }, [skipQuery]);
+
   if (error) return <div>...error</div>;
   if (isLoading) return <div>...loading</div>;
   return (
     <div className="w-full flex items-center align-middle justify-around h-full">
       <button onClick={backward}>BACKWARD</button>
-      {data &&
-        data.data.map((image: { url: string }, index: number) => {
+      {loading &&
+        data?.data.map((image: { url: string }, index: number) => {
           return (
-            <div key={index}>
-              <Image src={image.url} width={300} height={300} quality={100} />
+            <div className={`${animation}`} key={index}>
+              <Image
+                className={`${animation}`}
+                src={image.url}
+                width={300}
+                height={300}
+                quality={100}
+              />
             </div>
           );
         })}
