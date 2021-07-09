82fdd7f7ce4064031831b6d9ef7f4d61c496ab21
viccabezos <cabezosvic@gmail.com>
2021-07-09 15:16:24 +0200

    add component amenities

--- components/propertypage/amenities.tsx
+++ components/propertypage/amenities.tsx
@@ -1,26 +1,23 @@
 import wetsuit from "../../public/Images/wetsuit.svg";
 import surfboard from "../../public/Images/surfboard.svg";
 import shower from "../../public/Images/shower.svg";
-import party from "../../public/Images/party.svg"
-import noPartyAllowed from "../../public/Images/noparty.svg"
-import noPetAllowed from "../../public/Images/noPets.svg"
-import Wc from '@material-ui/icons/WcOutlined';
-import Bathtub from '@material-ui/icons/BathtubOutlined';
-import Kitchen from '@material-ui/icons/KitchenOutlined';
-import Laundry from '@material-ui/icons/LocalLaundryServiceOutlined';
- import Wifi from '@material-ui/icons/WifiOutlined';
-import Restaurant from '@material-ui/icons/RestaurantMenuOutlined';
-import Bike from '@material-ui/icons/DirectionsBikeOutlined';
- import Checkin from '@material-ui/icons/Restore';
-import Checkout from '@material-ui/icons/Update';
-import NoSmoke from '@material-ui/icons/SmokeFree';
-import Smoking from '@material-ui/icons/SmokingRooms';
-import PetsIcon from '@material-ui/icons/Pets';
-import NotInterestedIcon from '@material-ui/icons/NotInterested';
+import party from "../../public/Images/party.svg";
+import noPartyAllowed from "../../public/Images/noparty.svg";
+import noPetAllowed from "../../public/Images/noPets.svg";
+import Wc from "@material-ui/icons/WcOutlined";
+import Bathtub from "@material-ui/icons/BathtubOutlined";
+import Kitchen from "@material-ui/icons/KitchenOutlined";
+import Laundry from "@material-ui/icons/LocalLaundryServiceOutlined";
+import Wifi from "@material-ui/icons/WifiOutlined";
+import Restaurant from "@material-ui/icons/RestaurantMenuOutlined";
+import Bike from "@material-ui/icons/DirectionsBikeOutlined";
+import Checkin from "@material-ui/icons/Restore";
+import Checkout from "@material-ui/icons/Update";
+import NoSmoke from "@material-ui/icons/SmokeFree";
+import Smoking from "@material-ui/icons/SmokingRooms";
+import PetsIcon from "@material-ui/icons/Pets";
+import NotInterestedIcon from "@material-ui/icons/NotInterested";
 
 export default function amenities(): JSX.Element {
-  return (
-   
-  )
-  
-};
+  return <div>Hello</div>;
+}
